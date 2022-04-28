f05e8f6e104e3ac71bb1ad1cb9064fd5bea20f22
spencerlii <spencer850922@gmail.com>
2022-04-28 03:50:59 +0200

    Update Exercise_4.java

--- src/main/java/exercise_4/Exercise_4.java
+++ src/main/java/exercise_4/Exercise_4.java
@@ -53,8 +53,8 @@ public class Exercise_4 {
 
 			GraphFrame gf = GraphFrame.apply(vertices,edges);
 
-			List<Double> prob = List.of(0.1, 0.3, 0.5);
-			List<Integer> max_it = List.of(5, 10, 15);
+			List<Double> prob = List.of(0.15, 0.25, 0.4);
+			List<Integer> max_it = List.of(4, 5, 6, 7, 8, 9, 10, 11, 12);
 
 			for (Double i : prob) {
 				for (Integer j : max_it) {
